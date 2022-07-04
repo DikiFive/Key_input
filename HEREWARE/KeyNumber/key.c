490dbaccd1cc6ed2f324bc2c58ebd5363015e891
DikiFive <Diki5kg@163.com>
2022-07-05 07:59:51 +0800

    change

--- HEREWARE/KeyNumber/key.c
+++ HEREWARE/KeyNumber/key.c
@@ -4,14 +4,16 @@ void Key_int(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); //使能GPIOB端口时钟
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOE, ENABLE); //使能PORTA，PORTE时钟
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;         // BEEP-->PB.8 端口配置
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;  //推挽输出
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; //速度为50MHz
-    GPIO_Init(GPIOB, &GPIO_InitStructure);            //根据参数初始化GPIOB.8
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_3; // KEY0-KEY1
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;          //设置成上拉输入
+    GPIO_Init(GPIOE, &GPIO_InitStructure);                 //初始化GPIOE4,3
 
-    GPIO_ResetBits(GPIOB, GPIO_Pin_8); //初始化输出为0，关闭蜂鸣器
+    //初始化 WK_UP -->GPIOA.0   下拉输入
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; // PA0设置成输入，默认下拉
+    GPIO_Init(GPIOA, &GPIO_InitStructure);        //初始化GPIOA.0
 }
 
 u8 KEY_Scan(u8 mode)
--- USER/main.c
+++ USER/main.c
@@ -5,12 +5,29 @@
 #include "key.h"
 int main(void)
 {
+	vu8 key = 0;
 	delay_init();
 	LED_Init();
 	Key_int();
 	Beep_int();
+	LED0 = 0;
 	while (1)
 	{
-		
+		key = KEY_Scan(0);
+		if (key)
+		{
+			switch (key)
+			{
+			case WKUP_PRES: //控制蜂鸣器
+				BEEP = !BEEP;
+				break;
+			case KEY1_PRES: //¿ØÖÆLED1·­×ª
+				LED1 = !LED1;
+			case KEY0_PRES: //Í¬Ê±¿ØÖÆLED0,LED1·­×ª
+				LED0 = !LED0;
+				LED1 = !LED1;
+				break;
+			}
+		}
 	}
 }
