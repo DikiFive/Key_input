00cea468cc49d8475cb2934009940b53d181a47d
DikiFive <Diki5kg@163.com>
2022-07-05 09:36:03 +0800

    change

--- USER/main.c
+++ USER/main.c
@@ -3,17 +3,18 @@
 #include "led.h"
 #include "beep.h"
 #include "key.h"
+
 int main(void)
 {
 	vu8 key = 0;
-	delay_init();
-	LED_Init();
-	Key_int();
-	Beep_int();
-	LED0 = 0;
+	delay_init(); //延时函数初始化
+	LED_Init();	  //初始化与LED连接的硬件接口
+	Key_int();	  //初始化与按键连接的硬件接口
+	Beep_int();	  //初始化蜂鸣器端口
+	LED0 = 0;	  //先点亮红灯
 	while (1)
 	{
-		key = KEY_Scan(0);
+		key = KEY_Scan(0); //得到键值
 		if (key)
 		{
 			switch (key)
@@ -21,13 +22,18 @@ int main(void)
 			case WKUP_PRES: //控制蜂鸣器
 				BEEP = !BEEP;
 				break;
-			case KEY1_PRES: //¿ØÖÆLED1·­×ª
+			case KEY1_PRES: //控制LED1翻转
 				LED1 = !LED1;
-			case KEY0_PRES: //Í¬Ê±¿ØÖÆLED0,LED1·­×ª
+			case KEY0_PRES: //同时控制LED0，LED1翻转
 				LED0 = !LED0;
 				LED1 = !LED1;
 				break;
 			}
 		}
+		else
+		{
+			delay_ms(10);
+		}
 	}
 }
+
